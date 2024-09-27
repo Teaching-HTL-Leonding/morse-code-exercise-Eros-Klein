97620f3fab6bba485279a1e683ebfccf5eecc45d
eros-klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-09-27 11:35:41 +0200

    configured routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {EncryptComponent} from "./encrypt/encrypt.component";
+import {DecryptComponent} from "./decrypt/decrypt.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/encrypt', pathMatch: 'full' },
+  { path: 'encrypt', component: EncryptComponent },
+  { path: 'decrypt', component: DecryptComponent }
+];
