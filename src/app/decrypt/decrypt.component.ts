b632f89c004ea59e55fef71514578da81fa9089d
eros-klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-09-27 11:32:49 +0200

    initiated project

---
+++ src/app/decrypt/decrypt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decrypt',
+  standalone: true,
+  imports: [],
+  templateUrl: './decrypt.component.html',
+  styleUrl: './decrypt.component.css'
+})
+export class DecryptComponent {
+
+}
---
+++ src/app/encrypt/encrypt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encrypt',
+  standalone: true,
+  imports: [],
+  templateUrl: './encrypt.component.html',
+  styleUrl: './encrypt.component.css'
+})
+export class EncryptComponent {
+
+}
